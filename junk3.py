8cd83bab39b2e59b5bfc96ce460e8f7e0217cf62
Emre <m.emrepolat@icloud.com>
2023-03-23 03:06:17 +0300

    program_ac metotu denemsi
    metot yol girilen dosyanın açılmasını sağlamalı, dosyanın uzantısına gerek kalmadan halledilebilmeli daha kullanışlı bir yöntem olacaktır

---
+++ junk3.py
@@ -0,0 +1,29 @@
+import os
+from FileOperations import MoveOperations
+
+def program_ac(dosya_yolu):
+    try:
+        if os.path.exists(dosya_yolu):  # Dosya var mı kontrolü yapılıyor
+
+            file_name_inPath = dosya_yolu.split('/')[-1]
+            if '.' in file_name_inPath:
+                file_name_inPath = file_name_inPath.split('.')[0]
+            files = MoveOperations.get_file_names_inFolder(os.path.dirname(path))
+            if file_name_inPath in files:
+                os.startfile(dosya_yolu)
+        else:
+            return "Belirtilen dosya yolu geçerli değil."
+
+    except:
+        return "Program açılırken bir hata oluştu."
+
+
+path = "C:/Users/Emre/Desktop/sosyal-guvenlik-4a-ise-giris-cikis-bildirgesi"
+
+
+
+
+
+program_ac(f"{path}")  # Internet Explorer açar
+
+#print(os.path.dirname(path))
