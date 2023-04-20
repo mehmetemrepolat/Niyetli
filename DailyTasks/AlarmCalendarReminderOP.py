e16f3cb5e6302ea6c19885163f5bf0a81a4a1803
Emre <m.emrepolat@icloud.com>
2023-04-20 23:14:58 +0300

    MediaControl ve AlarmCalendarReminderOP Class yapısı düzenleme

--- DailyTasks/AlarmCalendarReminderOP.py
+++ DailyTasks/AlarmCalendarReminderOP.py
@@ -6,54 +6,55 @@ import sys
 import os
 import time
 
+class DateOperations:
 
-def get_date(today_d=date.today()):
-    if today_d != date.today():
-        return date.today() + timedelta(today_d)
-    else:
-        return today_d
-
-
-def get_date_w_string(date_input):
-    # date = date.strftime()
-    return date_input.strftime("%B %d, %Y")
+    def get_date(self, today_d=date.today()):
+        if today_d != date.today():
+            return date.today() + timedelta(today_d)
+        else:
+            return today_d
 
 
-def get_date_only_number(date_input):
-    return date_input.split()[1].replace(",", "")
+    def get_date_w_string(self, date_input):
+        # date = date.strftime()
+        return date_input.strftime("%B %d, %Y")
 
 
-def show_balloon(title, msg):
-    message_map = {
-        win32con.WM_DESTROY: OnDestroy,
-    }
-    wc = WNDCLASS()
-    hinst = wc.hInstance = GetModuleHandle(None)
-    wc.lpszClassName = "PythonTaskbar"
-    wc.lpfnWndProc = message_map
-    classAtom = RegisterClass(wc)
-    style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-    hwnd = CreateWindow(classAtom, "Taskbar", style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, hinst, None)
-    UpdateWindow(hwnd)
-    iconPathName = os.path.abspath(os.path.join(sys.path[0], "Niyetli.ico"))
-    icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
-    try:
-        hicon = LoadImage(hinst, iconPathName, win32con.IMAGE_ICON, 0, 0, icon_flags)
-    except:
-        hicon = LoadIcon(0, win32con.IDI_APPLICATION)
-    flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-    nid = (hwnd, 0, flags, win32con.WM_USER + 20, hicon, "tooltip")
-    Shell_NotifyIcon(NIM_ADD, nid)
-    Shell_NotifyIcon(NIM_MODIFY, (hwnd, 0, NIF_INFO, win32con.WM_USER + 20, hicon, "Balloon  tooltip", msg, 200, title))
-
-    time.sleep(10)
-    DestroyWindow(hwnd)
+    def get_date_only_number(self, date_input):
+        return date_input.split()[1].replace(",", "")
 
 
-def OnDestroy(hwnd, msg, wparam, lparam):
-    nid = (hwnd, 0)
-    Shell_NotifyIcon(NIM_DELETE, nid)
-    PostQuitMessage(0)
+    def show_balloon(self, title, msg):
+        message_map = {
+            win32con.WM_DESTROY: OnDestroy,
+        }
+        wc = WNDCLASS()
+        hinst = wc.hInstance = GetModuleHandle(None)
+        wc.lpszClassName = "PythonTaskbar"
+        wc.lpfnWndProc = message_map
+        classAtom = RegisterClass(wc)
+        style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+        hwnd = CreateWindow(classAtom, "Taskbar", style, 0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, 0, 0, hinst, None)
+        UpdateWindow(hwnd)
+        iconPathName = os.path.abspath(os.path.join(sys.path[0], "Niyetli.ico"))
+        icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+        try:
+            hicon = LoadImage(hinst, iconPathName, win32con.IMAGE_ICON, 0, 0, icon_flags)
+        except:
+            hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+        flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+        nid = (hwnd, 0, flags, win32con.WM_USER + 20, hicon, "tooltip")
+        Shell_NotifyIcon(NIM_ADD, nid)
+        Shell_NotifyIcon(NIM_MODIFY, (hwnd, 0, NIF_INFO, win32con.WM_USER + 20, hicon, "Balloon  tooltip", msg, 200, title))
+
+        time.sleep(10)
+        DestroyWindow(hwnd)
+
+
+    def OnDestroy(self, hwnd, msg, wparam, lparam):
+        nid = (hwnd, 0)
+        Shell_NotifyIcon(NIM_DELETE, nid)
+        PostQuitMessage(0)
 
 
 # show_balloon("Niyetli'den mesaj var!", "Hatalar düzeltildi, Performans iyileştirmeleri yapıldı, Kedi videolarıyla kalpler eridi.")
--- WindowControls/MediaControl.py
+++ WindowControls/MediaControl.py
@@ -2,34 +2,34 @@ import ctypes
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
-
-def set_volume(volume):  # Bu kısım daha sonrasında sesli komutlara göre uyumlu hale getirilecek
-    """Ses Yükseklik Kontrolleri"""
-    if volume < 0 or volume > 100:
-        print("0-100 arasında bir sayı girin!")
-    else:
-        devices = AudioUtilities.GetSpeakers()
-        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-        volume_object = ctypes.cast(interface, ctypes.POINTER(IAudioEndpointVolume))
-        volume_object.SetMasterVolumeLevelScalar(volume / 100, None)
-
-
-def play_pause_media():
-    """Medya durdur/oynat."""
-    ctypes.windll.user32.keybd_event(0xB3, 0, 0, 0)  # Play/Pause tuşu
-    ctypes.windll.user32.keybd_event(0xB3, 0, 0x0002, 0)
+class MediaControl:
+    def set_volume(self, volume):  # Bu kısım daha sonrasında sesli komutlara göre uyumlu hale getirilecek
+        """Ses Yükseklik Kontrolleri"""
+        if volume < 0 or volume > 100:
+            print("0-100 arasında bir sayı girin!")
+        else:
+            devices = AudioUtilities.GetSpeakers()
+            interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+            volume_object = ctypes.cast(interface, ctypes.POINTER(IAudioEndpointVolume))
+            volume_object.SetMasterVolumeLevelScalar(volume / 100, None)
 
 
-def next_media():
-    """Sonraki medya parçası"""
-    ctypes.windll.user32.keybd_event(0xB0, 0, 0, 0)  # Forward tuşu
-    ctypes.windll.user32.keybd_event(0xB0, 0, 0x0002, 0)
+    def play_pause_media(self):
+        """Medya durdur/oynat."""
+        ctypes.windll.user32.keybd_event(0xB3, 0, 0, 0)  # Play/Pause tuşu
+        ctypes.windll.user32.keybd_event(0xB3, 0, 0x0002, 0)
 
 
-def previous_media():
-    """Önceki medya parçası"""
-    ctypes.windll.user32.keybd_event(0xB1, 0, 0, 0)  # Backward tuşu
-    ctypes.windll.user32.keybd_event(0xB1, 0, 0x0002, 0)
+    def next_media(self):
+        """Sonraki medya parçası"""
+        ctypes.windll.user32.keybd_event(0xB0, 0, 0, 0)  # Forward tuşu
+        ctypes.windll.user32.keybd_event(0xB0, 0, 0x0002, 0)
+
+
+    def previous_media(self):
+        """Önceki medya parçası"""
+        ctypes.windll.user32.keybd_event(0xB1, 0, 0, 0)  # Backward tuşu
+        ctypes.windll.user32.keybd_event(0xB1, 0, 0x0002, 0)
 
 #set_volume(50)
 # next_media()
